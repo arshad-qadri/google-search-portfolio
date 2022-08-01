a72cd114614ea3d5c55ecd2f4758998d33bd232b
arshad-qadri <arshadqadri321@gmail.com>
2022-08-01 04:54:21 +0000

    google main page done

--- Layouts/Footer.js
+++ Layouts/Footer.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <>
-    <div className="footer">
-      <div>India</div>
-      <div>
-
-        
+      <div className="footer">
+        <div className="footer-row-1">India</div>
+        <div className="footer-row-2">
+          <ul>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Advertising</a>
+            </li>
+            <li>
+              <a href="#">Business</a>
+            </li>
+            <li>
+              <a href="#">How search works</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="#">Privacy</a>
+            </li>
+            <li>
+              <a href="#">Terms</a>
+            </li>
+            <li>
+              <a href="#">Settings</a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-      
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
